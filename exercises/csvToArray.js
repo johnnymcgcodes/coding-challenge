ada73e247879b4ef8b49ed0a1351ccadafe19ded
Johnny McG <johnnymcg@outlook.com>
2023-04-06 12:30:34 -0700

    csvToArray function first commit

--- exercises/csvToArray.js
+++ exercises/csvToArray.js
@@ -16,4 +16,24 @@ module.exports.run = function(csv){
 	Write your code below the comment.
 */
 
+const [headers, ...row_values] = convertCSVTo2DArray(csv);
+
+const arrayObjects = convertNestedArraysToObjects(headers, row_values);
+
+return arrayObjects;
+};
+
+function convertCSVTo2DArray(csv){
+	return csv
+    .trim()
+    .split("/n")
+    .map((val) => val.split(","));
+}
+
+function convertNestedArraysToObjects(headers, row_values) {
+  return row_values.map((item) => {
+    let object = {};
+    headers.forEach((key, index) => (object[key] = item[index]));
+    return object;
+  });
 };
