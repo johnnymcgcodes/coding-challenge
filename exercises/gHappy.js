b5a2fc94741752813119c60c470636b93d672b8d
Johnny McG <johnnymcg@outlook.com>
2023-04-06 14:24:25 -0700

    gHappy function first commit

--- exercises/gHappy.js
+++ exercises/gHappy.js
@@ -9,5 +9,22 @@ Return true if all the g's in the given string are happy, otherwise return false
 
         Write your code below the comment.
 */
-
+if (typeof str !== "string") {
+  throw new Error("Input must be a string");
+}
+try {
+  if (str.length === 0) {
+    return "Empty string entered";
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "g") {
+      if (str[i - 1] !== "g" && str[i + 1] !== "g") {
+        return false;
+      }
+    }
+  }
+  return true;
+} catch (error) {
+  return "Error: " + error.message;
+}
 };
