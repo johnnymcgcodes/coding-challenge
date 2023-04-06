82e5fbd484b037af0dc3bab7756a219312907abc
Johnny McG <johnnymcg@outlook.com>
2023-04-06 15:46:14 -0700

    loneSome function initial commit

--- exercises/loneSome.js
+++ exercises/loneSome.js
@@ -10,5 +10,24 @@ Examples:
 Write your code below the comment.
 
 */
+try {
+  var sum_unique_nums = 0;
+  var number_container = {};
+  for (var i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] !== "number") {
+      throw new Error("All arguments must be numbers");
+    }
+		// add number to number_container and update count of that number
+    number_container[arguments[i]] = 1 + (number_container[arguments[i]] || 0);
+  }
 
+  var numsWithOneCount = Object.keys(number_container).filter(function (key) {
+    return number_container[key] === 1;
+  });
+  numsWithOneCount.forEach((integer) => (sum_unique_nums += parseInt(integer)));
+  return sum_unique_nums;
+} catch (error) {
+  return "Error: " + error.message;
+}
 };
+
