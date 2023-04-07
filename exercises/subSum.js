c1de53583cc4c961b6a2247f17bfc53adc9fed2b
Johnny McG <johnnymcg@outlook.com>
2023-04-07 10:15:39 -0700

    subSum function initial commit

--- exercises/subSum.js
+++ exercises/subSum.js
@@ -1,6 +1,6 @@
 
 module.exports.run = function(x, y) {
-/*
+  /*
 	You are given a 2D array. Using the x and y co-ordinates, find the sum of all the numbers from (0, 0) to (x, y)
 
 	Example:
@@ -11,11 +11,32 @@ module.exports.run = function(x, y) {
 
 	 Write your code below the comment.
 */
-const twoDArray = [[0,  4,  2,  1],
-		   [3, -1,  1,  0],
-		   [0,  0,  2,  3],
-		   [1,  3, -3,  0]];
-
+  const twoDArray = [
+    [0, 4, 2, 1],
+    [3, -1, 1, 0],
+    [0, 0, 2, 3],
+    [1, 3, -3, 0],
+  ];
+	validateInputBetweenZeroAndArraySize(x, twoDArray[0].length-1);
+	validateInputBetweenZeroAndArraySize(y, twoDArray.length-1);
+  var sum = 0;
+  for (let i = 0; i <= y; i++) {
+    for (let j = 0; j <= x; j++) {
+      sum += twoDArray[i][j];
+    }
+  }
+  return sum;
 };
 
+function validateInputBetweenZeroAndArraySize(value, array_length) {
+  if (typeof value !== "number") {
+    throw new Error(`Value: ${value} must be a number`);
+  }
+  if (!Number.isInteger(value)) {
+    throw new Error(`Value: ${value} must be an integer`);
+  }
+  if (value < 0 || value > array_length) {
+    throw new Error(`Value: ${value} must be between 0 and ${array_length} (inclusive)`);
+  }
+}
 
